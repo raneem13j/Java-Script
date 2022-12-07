66acbc6d53ad702700f6487de15ca123caf80ac1
Raneem <aljamal.raneem@gmail.com>
2022-12-07 21:24:29 +0200

    step_7

--- javascript-basics-01/step_7.js
+++ javascript-basics-01/step_7.js
@@ -1,10 +1,10 @@
-var shoe = prompt("Enter your shoe size");
-var birth = prompt("Enter your birth year");
-
-function multipleOperarions(){
-  const multiply = shoe * 2;
-  const add = multiply
-  
-      
-
+document.getElementById("validate").onclick = function(){
+  var shoe = document.getElementById("shoe_size").value;
+  var year = document.getElementById("year").value;
+  var multi1 = shoe * 2;
+  var adds = multi1 + 5;
+  var multi2 = adds * 50;
+  var subtract = multi2 - year;
+  var add = subtract + 1766;
+  alert(add);
 }
