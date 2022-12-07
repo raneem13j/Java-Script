8cb8bd46034d993ed480e1313e869d866524b805
Raneem <aljamal.raneem@gmail.com>
2022-12-07 13:02:03 +0200

    step_4

---
+++ javascript-basics-01/step_4.js
@@ -0,0 +1,8 @@
+var user = prompt("Enter your name", "name...");
+
+var surname = prompt("Enter your surname", "surname...");
+
+var city = prompt("Enter your city", "city...");
+
+alert("Hello " + user + " " + surname + " from " + city);
+
