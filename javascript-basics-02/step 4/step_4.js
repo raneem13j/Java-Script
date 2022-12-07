b3c6c7c5df8fb79cbd8f97ba69bedc8514dfb066
Raneem <aljamal.raneem@gmail.com>
2022-12-08 01:52:42 +0200

    step_4

---
+++ javascript-basics-02/step 4/step_4.js
@@ -0,0 +1,10 @@
+
+let reset = document.querySelector("button").addEventListener("click", function(){
+    var user = document.getElementById("name").value = "";
+ 
+    var surname = document.getElementById("surname").value = "";
+     
+    var city = document.getElementById("city").value = "";
+   
+    
+})
