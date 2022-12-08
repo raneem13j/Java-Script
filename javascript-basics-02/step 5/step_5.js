9b58b26081cc8ee27fbe10368380e842762f0314
Raneem <aljamal.raneem@gmail.com>
2022-12-08 02:25:30 +0200

    step_5

---
+++ javascript-basics-02/step 5/step_5.js
@@ -0,0 +1,10 @@
+let image = document.querySelector(".container").addEventListener("mouseover", function(){
+    document.querySelector("#image1").src="images/image1_2.jpg"
+    document.querySelector("#image2").src="images/image2_2.jpg"
+    document.querySelector("#image3").src="images/image3_2.jpg"
+    document.querySelector("#image4").src="images/image4_2.jpg"
+    document.querySelector("#image5").src="images/image5_2.jpg"
+ })
+ 
+
+
