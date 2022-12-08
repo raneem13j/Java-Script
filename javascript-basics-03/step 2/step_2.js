8ff738c4aded269ac11565da81fed903f949c921
Raneem <aljamal.raneem@gmail.com>
2022-12-08 10:49:09 +0200

    step_2

---
+++ javascript-basics-03/step 2/step_2.js
@@ -0,0 +1,8 @@
+   function hideshow(num){
+    var text = document.getElementById("texte");
+    if (num === "show") {
+        text.style.display = "block";
+    }else if (num === "hide") {
+        text.style.display = "none";
+    }
+   } 
